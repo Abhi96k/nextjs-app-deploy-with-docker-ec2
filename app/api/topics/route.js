62facd8fbfcdee270b1beb0c41f4118ed23c3261
abhi@96k <abhisheknangare96k@gmail.com>
2024-10-13 19:46:55 +0530

    Refactor: Improve formatting of JSON response in POST endpoint

--- app/api/topics/route.js
+++ app/api/topics/route.js
@@ -6,7 +6,12 @@ export async function POST(request) {
   const { title, description } = await request.json();
   await connectMongoDB();
   await Topic.create({ title, description });
-  return NextResponse.json({ message: "Topic Created" }, { status: 201 });
+  return NextResponse.json(
+    {
+      message: "Topic Created",
+    },
+    { status: 201 }
+  );
 }
 
 export async function GET() {
